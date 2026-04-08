552ddd033bf32527ea712fc08cc751e398a44695
Marylyn <marylynokotta@gmail.com>
2026-04-08 22:48:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const apiKey = "1374f71facc063253446d0f0f244061e";
+        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+        const searchBox =document.querySelector(".search-row input");
+
+        const searchBtn =document.querySelector(".search-row button");
+
+        async function getWeather(city){
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            var data = await response.json();
+
+            console.log(data);
+
+            
+            document.querySelector("#city").innerHTML = data.name;
+            document.querySelector("#temp").innerHTML = Math.round(data.main.temp) + "°C";
+            document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector("#wind").innerHTML = data.wind.speed + " km/h";
+
+        }
+        searchBtn.addEventListener("click",()=>{
+            getWeather(searchBox.value);
+
+        })
+
+      
+
+        getWeather();
+
+        
+        
+
